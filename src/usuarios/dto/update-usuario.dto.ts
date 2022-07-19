26e8b12765080afacd2688ed53b51df0ce81bb42
pedrol2301 <ppriqq@gmail.com>
2022-07-18 23:53:20 -0300

    Terminando rota patch usuarios

--- src/usuarios/dto/update-usuario.dto.ts
+++ src/usuarios/dto/update-usuario.dto.ts
@@ -1,4 +1,31 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUsuarioDto } from './create-usuario.dto';
+import { ArrayMinSize, IsArray, IsBoolean, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
-export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {}
+export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {
+    @IsString()
+    public nome: string;
+
+
+    @IsString()
+    public telefone: string;
+
+
+    @IsNumber()
+    @IsNotEmpty()
+    public cpf: string;
+
+    @IsString()
+    public endereco: string;
+
+    @IsString()
+    @IsNotEmpty()
+    public email: string;
+
+    @IsNotEmpty()
+    public senha: string;
+
+    @IsString()
+    @IsNotEmpty()
+    public perfil: string;
+}
--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -1,7 +1,7 @@
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 import { Knex } from 'knex';
-import { hash } from 'bcrypt';
+import { hash, compare } from 'bcrypt';
 
 import {
   HttpException,
@@ -35,7 +35,14 @@ export class UsuariosService {
 
   async findAll() {
     try {
-      const usuarios = await this.knex.table('usuario');
+      const usuarios = await this.knex.select([
+      "nome",
+      "cpf",
+      "email",
+      "endereco",
+      "telefone",
+      "perfil",
+    ]).table('usuario');
       return usuarios;
     } catch (error) {
       console.log(error);
@@ -44,15 +51,75 @@ export class UsuariosService {
 
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} usuario`;
+  async findOne(id: number) {
+    try {
+      const usuario = await this.knex.select([
+        "nome",
+        "cpf",
+        "email",
+        "endereco",
+        "telefone",
+        "perfil",
+      ]).table('usuario').where({"idusuario":id});
+      return usuario;
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(error, HttpStatus.BAD_REQUEST);
+    }
   }
 
-  update(id: number, updateUsuarioDto: UpdateUsuarioDto) {
-    return `This action updates a #${id} usuario`;
+  async update(id: number, updateUsuarioDto: UpdateUsuarioDto) {
+    var updateFields;
+    try {
+
+      const usuario = await this.knex.table('usuario').where({idusuario:id});
+
+      updateFields = await this.validarDiferenca(usuario[0],updateUsuarioDto);
+      console.log (updateFields.length);
+      console.log (updateFields);
+      if(updateFields.length > 0){
+        if(updateFields['senha']){
+          updateFields['senha'] = await hash(updateUsuarioDto.senha,10);
+          console.log(updateFields['senha']);
+          await this.knex.update(updateFields).where({idusuario:id}).table('usuario');
+          return `Usuario atualizado!`;
+        }else{
+          await this.knex.update(updateFields).where({idusuario:id}).table('usuario');
+          return `Usuario atualizado!`;
+        }
+      }else{
+        return `Não houve alterações!`;
+      }
+      
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(error, HttpStatus.BAD_REQUEST);
+    }
+    
   }
 
   remove(id: number) {
     return `This action removes a #${id} usuario`;
+  }
+  async validarDiferenca(arr1,arr2) {
+    var arr1Props = Object.getOwnPropertyNames(arr1);
+    var returnField = [];
+
+    for (var i = 0; i < arr1Props.length; i++) {
+      var propName = arr1Props[i];
+      
+      if (propName == 'senha') {
+        console.log('entrou')
+        let result = await compare(arr1[propName],arr2[propName]);
+        
+        if(!result)
+          returnField[propName] = arr2[propName];
+      }else if (arr1[propName] !== arr2[propName] && propName != 'senha' && arr2[propName] != undefined) {
+        returnField[propName] = arr2[propName];
+      }
+
+
+    }
+    return returnField;
   }
 }
